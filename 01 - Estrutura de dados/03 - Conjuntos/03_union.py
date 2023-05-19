4b3ad14cdef7359e08b228ef0952e0264b7483c6
kirestein <erik.proenca2011@gmail.com>
2023-05-19 15:53:51 -0300

    anotation about code

--- 01 - Estrutura de dados/03 - Conjuntos/03_union.py
+++ 01 - Estrutura de dados/03 - Conjuntos/03_union.py
@@ -1,3 +1,6 @@
+# Union - União de conjuntos
+# A união de conjuntos é uma operação que retorna um novo conjunto com todos os elementos que pertencem a pelo menos um dos conjuntos, sem repetir os conjuntos que aprece nos dois conjuntos.
+
 conjunto_a = {1, 2}
 conjunto_b = {3, 4}
 
--- teste.py
+++ teste.py
@@ -1,2 +1,5 @@
 [print(n**2) if n > 6 else print(n) for n in range(10) if n % 2 == 0]
 
+carro = {"marca": "Fiat", "modelo": "palio", "placa": "ABD-9826"}
+carro.get("motor")
+print( carro.get("motor"))
